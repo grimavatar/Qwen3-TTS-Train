eb17638a0c4fbfdea288d5abead1eed1b9a49d99
grimavatar <57611284+grimavatar@users.noreply.github.com>
2026-04-07 07:29:48 -0400

    Update modeling_qwen3_tts.py

--- qwen_tts/core/models/modeling_qwen3_tts.py
+++ qwen_tts/core/models/modeling_qwen3_tts.py
@@ -1633,8 +1633,12 @@ class Qwen3TTSTalkerForConditionalGeneration(Qwen3TTSTalkerTextPreTrainedModel, 
                 sub_talker_inputs_embeds.append(self.code_predictor.get_input_embeddings()[i-1](codec_ids[:, i:i+1]))
         sub_talker_inputs_embeds = torch.cat(sub_talker_inputs_embeds, dim=1)
         
-        sub_talker_outputs = self.code_predictor.forward_finetune(inputs_embeds=sub_talker_inputs_embeds,
-                                                                 labels=codec_ids[:, 1:])
+        # https://github.com/QwenLM/Qwen3-TTS/issues/179#issuecomment-4108009116
+        sub_talker_outputs = self.code_predictor.forward_finetune(
+            inputs_embeds=sub_talker_inputs_embeds,
+            labels=codec_ids[:, 1:],
+            shift_labels=codec_ids[:, 1:].contiguous(),  # Fix
+        )
         
         sub_talker_logits = sub_talker_outputs.logits
         sub_talker_loss = sub_talker_outputs.loss
